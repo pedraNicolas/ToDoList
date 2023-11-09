e9e19421763ea30aca9418e3487e9c4f73dba5dd
pedraNicolas <nicolas.pedra@sima.ag>
2023-11-09 18:57:47 -0300

    Initial Commit

---
+++ HelloXcode/HelloXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeApp.swift
+//  HelloXcode
+//
+//  Created by Nicolas Pedra on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
